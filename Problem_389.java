f969a5f0815e9f07b4d81bb06d07f7ed498d0a5e
anand-saurabh <saurabh.anand@ufl.edu>
2020-05-18 17:12:00 -0400

    find the difference

---
+++ Problem_389.java
@@ -0,0 +1,38 @@
+// problem : 389. Find the Difference
+
+class Solution {
+    public char findTheDifference(String s, String t) {
+        
+        Map<Character, Integer>
+            charsPresentMap = new HashMap();
+        char key = '0';
+        int freq = 0;
+        for (int i = 0; i < s.length(); i++)
+        {
+            key = s.charAt(i);
+            freq = charsPresentMap.getOrDefault(key, 0);
+            charsPresentMap.put(key, freq + 1);
+        }
+        for (int i = 0; i < t.length(); i++)
+        {
+            key = t.charAt(i);
+            if(charsPresentMap.containsKey(key))
+            {
+                freq = charsPresentMap.get(key);
+                charsPresentMap.put(key, freq - 1);
+            }
+            else
+            {
+                return key;
+            }
+        }
+        Set<Character>
+            set = charsPresentMap.keySet();
+        for (char c : set)
+        {
+            if(charsPresentMap.get(c) != 0)
+                return c;
+        }
+        return key;
+    }
+}
