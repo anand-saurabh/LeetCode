8a833d3f23f346ba970cf713632ad1419ec6ddb3
anand-saurabh <saurabh.anand@ufl.edu>
2020-05-24 14:54:44 -0400

    to lower

---
+++ Problem_709. To Lower Case.java
@@ -0,0 +1,19 @@
+//709. To Lower Case
+class Solution {
+    public String toLowerCase(String str) {
+        StringBuffer sb = new StringBuffer();
+        
+        int len = str.length();
+        char c;
+        for (int i = 0; i < len; i++)
+        {
+            c = str.charAt(i);
+            if(65 <= c  && c <= 91 )
+            {
+                c = (char)(str.charAt(i) + 32);
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
