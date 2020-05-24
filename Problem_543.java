be8047913853965e550f5ccbe7053668313c5389
anand-saurabh <saurabh.anand@ufl.edu>
2020-05-23 21:59:25 -0400

    diameter of tree

---
+++ Problem_543.java
@@ -0,0 +1,36 @@
+//Problem:543. Diameter of Binary Tree
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int max = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        if(root == null)
+            return 0;
+        findDia(root);
+        return max-1;
+    }
+    
+    int findDia(TreeNode node)
+    {
+        if(node == null)
+            return 0;
+        int left = 1 + findDia(node.left);
+        int right = 1 + findDia(node.right);
+        int m = left + right - 1;
+
+        if(m > max)
+        {
+            max = m;
+        }
+        return left > right ? left : right;
+        
+    }
+}
