583a75a7af1b02180fcf05f2d63b6aac7b491c87
anand-saurabh <saurabh.anand@ufl.edu>
2020-05-24 14:23:21 -0400

    dort by  parity

---
+++ Problem_905.Sort_Array_By_Parity.java
@@ -0,0 +1,28 @@
+905. Sort Array By Parity
+
+class Solution {
+    public int[] sortArrayByParity(int[] A) {
+        int i; int j = 0;
+        int len = A.length - 1;
+        i = len;
+        int temp;
+        while(j < i)
+        {
+        while(j < i && A[i] % 2 != 0)
+        {
+            i--;
+        }
+        while(j < i && A[j] % 2 == 0)
+        {
+            j++;
+        }
+         if(j < i)
+         {
+           temp = A[j];
+             A[j] = A[i];
+             A[i] = temp;
+         }
+        }
+        return A;
+    }
+}
