760ebb9a646951664913f06599c72c2ce1bd2857
anand-saurabh <saurabh.anand@ufl.edu>
2020-06-03 01:26:33 -0400

    course schedule

---
+++ Problem_207_Course_Schedule.java
@@ -0,0 +1,55 @@
+//207. Course Schedule
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        
+        Map<Integer, Set<Integer>>
+            courseMap = new HashMap();
+        
+        int row = prerequisites.length;
+        for (int i = 0; i < row; i++)
+        {
+            
+            for (int j = 0; j < 2; j++)
+            {
+                if(courseMap.containsKey(prerequisites[i][1]))
+                {
+                    Set<Integer> courses = courseMap.get(prerequisites[i][1]);
+                    while(!courses.isEmpty())
+                    {
+                        Set<Integer>
+                            temp = new HashSet();
+                        for (int cours : courses)
+                        {
+                            if(cours == prerequisites[i][0])
+                            {
+                                return false;
+                            }
+                            if(courseMap.containsKey(cours))
+                            {
+                                temp.addAll(courseMap.get(cours));
+                            }
+                        }
+                        courses = temp;
+                    }
+                        
+                }
+                if(courseMap.containsKey(prerequisites[i][0]))
+                {
+                    Set<Integer>
+                        cour = courseMap.get(prerequisites[i][0]);
+                    cour.add(prerequisites[i][1]);
+                    courseMap.put(prerequisites[i][0], cour);
+
+                }
+                else
+                {
+                    Set<Integer>
+                        cour = new HashSet();
+                    cour.add(prerequisites[i][1]);
+                    courseMap.put(prerequisites[i][0], cour);
+                }
+            }
+        }
+        return true;
+    }
+}
