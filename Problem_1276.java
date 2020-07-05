e3dc138c97e7057190b0e198920f5cf28d572ed8
anand-saurabh <saurabh.anand@ufl.edu>
2020-07-05 02:04:32 -0400

    num of burgesrs code

---
+++ Problem_1276.java
@@ -0,0 +1,8 @@
+//1276. Number of Burgers with No Waste of Ingredients
+class Solution {
+    public List<Integer> numOfBurgers(int tomatoSlices, int cheeseSlices) {
+        int twoX = tomatoSlices - 2 * cheeseSlices, x = twoX / 2, y = cheeseSlices - x;
+        return twoX >= 0 && twoX % 2 == 0 && y >= 0 ? Arrays.asList(x, y) : Arrays.asList();        
+
+    }
+}
