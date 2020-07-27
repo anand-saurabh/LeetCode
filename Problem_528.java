8d46af0f41bc3628c6706ba4851ccc5574f68379
anand-saurabh <saurabh.anand@ufl.edu>
2020-07-27 14:16:24 -0400

    random pick wt

---
+++ Problem_528.java
@@ -0,0 +1,18 @@
+class Solution {
+    private double[] probabilities;
+    
+    public Solution(int[] w) {
+        double sum = 0;
+        this.probabilities = new double[w.length];
+        for(int weight : w)
+            sum += weight;
+        for(int i = 0; i < w.length; i++){
+            w[i] += (i == 0) ? 0 : w[i - 1];
+            probabilities[i] = w[i]/sum; 
+        }
+    }
+     
+    public int pickIndex() {
+        return Math.abs(Arrays.binarySearch(this.probabilities, Math.random())) - 1;
+    }
+}
