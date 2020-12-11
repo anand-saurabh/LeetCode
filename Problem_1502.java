48ceb02a8ccfb1ee7df15c9e7e440fb22fbac453
anand-saurabh <saurabh789789@gmail.com>
2020-12-11 01:33:15 -0500

    problem solving

---
+++ Problem_1502.java
@@ -0,0 +1,11 @@
+//Problem: 1502. Can Make Arithmetic Progression From Sequence
+class Solution {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        Arrays.sort(arr);
+        int d = arr[1] - arr[0];
+        for(int i = 0; i < arr.length - 1; i++) 
+            if(arr[i + 1] - arr[i] != d)
+                return false;
+        return true;
+    }
+}
