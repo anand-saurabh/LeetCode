f2cf7c8614f1b61c95f0eaeb90d840f5f53ed1c6
Saurabh Anand <Saurabh789789@gmail.com>
2021-01-31 15:34:16 -0500

    Time: 5 ms (25.15%), Space: 49.3 MB (6.24%) - LeetHub

---
+++ two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        
+        Set<Integer>
+            sum = new HashSet();
+        return findIfExist(root, k, sum);
+    }
+    
+    boolean findIfExist(TreeNode node, int k, Set<Integer> sumSet)
+    {
+        if(node == null)
+            return false;
+        if(sumSet.contains(k - node.val))
+        {
+            return true;
+        }
+        else
+        {
+            sumSet.add(node.val);
+        }
+         
+        return findIfExist(node.left,k,sumSet) || findIfExist(node.right, k, sumSet);   
+        
+    }
+    
+    
+}
