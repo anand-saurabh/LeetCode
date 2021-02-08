1d9d735aad68adde214b0e9e6936db3b59659aee
Saurabh Anand <Saurabh789789@gmail.com>
2021-02-07 19:41:10 -0500

    Time: 15 ms (21.05%), Space: 47.6 MB (26.20%) - LeetHub

---
+++ find-the-town-judge/find-the-town-judge.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int findJudge(int N, int[][] trust) {
+        
+        int row = trust.length;
+        if(N == 1)
+        {
+            return N;
+        }
+        Map<Integer, Integer>
+            trustMap = new HashMap();
+        
+        Set<Integer>
+            others = new HashSet();
+        for (int i = 0; i < row; i++)
+        {
+            trustMap.put(trust[i][1], trustMap.getOrDefault(trust[i][1], 0) + 1);
+            others.add(trust[i][0]);
+        }
+        Set<Integer>
+            keys = trustMap.keySet();
+        
+        for (int key : keys)
+        {
+            
+            if(trustMap.get(key) == (N - 1) && !others.contains(key))
+            {
+                return key;
+            }
+        }
+        return -1;
+    }
+}
