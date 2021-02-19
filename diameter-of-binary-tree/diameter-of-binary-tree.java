cfd5b336f582ec1825c08f20261df31f665d9f34
Saurabh Anand <Saurabh789789@gmail.com>
2021-02-19 00:43:47 -0500

    Time: 0 ms (100.00%), Space: 38.8 MB (83.38%) - LeetHub

--- diameter-of-binary-tree/diameter-of-binary-tree.java
+++ diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -1,40 +1,44 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    int max = 0;
-    public int diameterOfBinaryTree(TreeNode root) {
-        findDia(root);
-        return max;
-    }
-    
-    int findDia(TreeNode node){
-        if(node == null)
-        {
-            return 0;
-        }
-        
-        int left = 1 + findDia(node.left);
-        int right = 1 + findDia(node.right);
-        
-        if((left + right - 2) > max)
-        {
-            max = left + right - 2;
-        }
-        
-        return Math.max(left, right);
-    }
-    
+    int max = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        
+        findDia(root);
+        return max;
+        
+    }
+    
+    int findDia(TreeNode node)
+    {
+        if(node == null)
+        {
+            return 0;
+        }
+        
+        int left = 1 + findDia(node.left);
+        int right = 1 + findDia(node.right);
+        if((left + right - 2) > max)
+        {
+            max = left + right - 2;
+        }
+        
+        return Math.max(left, right);
+        
+        
+        
+    }
 }
