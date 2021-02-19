1b4a4ac024755b5a92b481e21b21ca39ec85b733
Saurabh Anand <Saurabh789789@gmail.com>
2021-02-19 10:53:42 -0500

    Time: 5 ms (90.72%), Space: 39.9 MB (48.47%) - LeetHub

---
+++ linked-list-components/linked-list-components.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int numComponents(ListNode head, int[] G) {
+        Set<Integer>
+            set = new HashSet();
+        int count = 0;
+        for (int i = 0; i < G.length; i++)
+        {
+            set.add(G[i]);
+        }
+        ListNode cur = head;
+        boolean cont = false;
+        int ans = 0;
+        while(cur != null)
+        {
+  
+            if(set.contains(cur.val))
+            {
+                if(!cont)
+                {
+                    cont = true;
+                    ans += 1;
+                }
+            }
+            else
+            {
+                cont = false;
+            }
+            cur = cur.next;     
+        }
+        return ans;
+    }
+}
