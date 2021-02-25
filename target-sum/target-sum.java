c9e05135a4f2786fc22eddec32587d82d4ea0351
Saurabh Anand <Saurabh789789@gmail.com>
2021-02-25 01:17:53 -0500

    Time: 10 ms (71.07%), Space: 39.4 MB (19.87%) - LeetHub

--- target-sum/target-sum.java
+++ target-sum/target-sum.java
@@ -1,24 +1,34 @@
 class Solution {
-    int ans = 0;
-    public int findTargetSumWays(int[] nums, int S) {
-        if(nums.length == 0)
-            return ans;
-        find(nums, 0, -1, S);
-        return ans;
-        
-    }
-    
-    void find(int[] num, int sum, int i, int S)
-    {
-        if(sum == S && i == num.length - 1)
-        {
-            ans++;
-        }
-        
-        if(i < num.length - 1)
-        {
-        find(num, sum - num[i+1], i+1, S);
-        find(num, sum + num[i+1], i+1, S);
-        }
-    }
+    int mem[][];
+    public int findTargetSumWays(int[] nums, int S) {
+        mem = new int[nums.length][2001];
+        
+        for (int [] tem : mem)
+        {
+            Arrays.fill(tem, -1);
+        }
+        return findWays(nums, S, 0, 0);        
+    }
+    
+    int findWays(int [] nums, int s, int index, int cur)
+    {
+        if(index >= nums.length)
+        {
+            if(cur == s)
+            {
+                return 1;
+            }
+            return 0;
+        }
+        if(mem[index][cur + 1000] != -1)
+        {
+            return mem[index][cur + 1000];
+        }
+        
+        int a = findWays(nums, s, index + 1, cur + nums[index]);
+        int b = findWays(nums, s, index + 1, cur - nums[index]);
+        
+        mem[index][cur + 1000] = a + b;
+        return mem[index][cur + 1000];
+    }
 }
