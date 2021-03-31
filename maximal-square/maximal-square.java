1dbff17a7d9e6bc6c9f8ed1f15bdd0fd352b55b2
Saurabh Anand <Saurabh789789@gmail.com>
2021-03-30 23:35:06 -0400

    Time: 2 ms (99.82%), Space: 42.6 MB (24.07%) - LeetHub

--- maximal-square/maximal-square.java
+++ maximal-square/maximal-square.java
@@ -1,26 +1,45 @@
-class Solution {
+public class Solution {
     public int maximalSquare(char[][] input) {
-        
-        int row = input.length;
+               int row = input.length;
         int col = input[0].length;
+        int maxi = -1;
+        int maxj = -1;
+        int prev = 0;
 
         int max = 0;
 
-        int table[][] = new int[row + 1][col + 1];
+        int table[] = new int[col + 1];
+        int temp = 0;
 
         for (int i = 1; i <= row; i++) {
+            prev = table[0];
             for (int j = 1; j <= col; j++) {
                 if (input[i-1][j-1] == '1') {
-                    int min = Math.min(table[i - 1][j], table[i][j - 1]);
-                    min = Math.min(min, table[i - 1][j - 1]);
-                    table[i][j] = 1 + min;
-                    if (table[i][j] > max) {
-                        max = table[i][j];
+                    temp = table[j];
+                    table[j] = 1 + findMin(j, table, prev);
+                    if (table[j] > max) {
+                        max = table[j];
+                        maxi = i - 1;
+                        maxj = j - 1;
                     }
                 }
+                else
+                {
+                    table[j] = 0;
+                }
+
+                prev = temp;
+
             }
         }
-        return max * max;
         
+        return max * max;   
+    }
+    
+    static int findMin(int j, int table[], int prev)
+    {
+        int min = Math.min(table[j - 1], table[j]);
+        min = Math.min(min, prev);
+        return min;
     }
 }
